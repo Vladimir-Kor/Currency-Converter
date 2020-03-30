401bcb47e71e785356b5755d2e9a874f5a98b250
Kornienko Vladimir <54525073+Vladimir-Kor@users.noreply.github.com>
2020-03-30 20:14:57 +0300

    Update currency.js

--- currency.js
+++ currency.js
@@ -14,7 +14,7 @@ let options = {
     day: 'numeric',
     weekday: 'long'
 };
-   today.innerHTML = "To day: "+date.toLocaleString("en-US", options); 
+   today.innerHTML = "To day : "+date.toLocaleString("en-US", options); 
 
     $.ajax({
         type: 'get',
